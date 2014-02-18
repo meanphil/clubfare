72cccddfca00c2755b1aeb79fbd31eda8692c339
Phil Murray <pmurray@nevada.net.nz>
2014-02-19 09:35:05 +1300

    Re-arrange class methods, create Constants
    The class<<self style is just that, a style.

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -7,23 +7,28 @@ class Beer < ActiveRecord::Base
 	validates :name, length: { maximum: 254 }
 	
 	Wastage = 0.05
+	
+	TapMarkup = 2.811
+	OffLicenseMarkup = 2
 
-	def self.markup(method)
-		if method == "tap"
-			return 2.811
-		else
-			return 2
+	class <<self
+		def markup(method)
+			if method == "tap"
+				return TapMarkup
+			else
+				return OffLicenseMarkup
+			end
+		end
+	
+		def ontap
+			self.joins(:location).where(locations: { status: ['LOW','SERVING'] }).includes(:brewer)
 		end
 	end
-
-
+	
+	
 	def pricefor(serving, method)
 		volume = self.format.size
 		(self.price / (volume - (volume * Wastage)) / 1000 * serving * self.class.markup(method)
-	end
-
-	def self.ontap
-		self.joins(:location).where(locations: { status: ['LOW','SERVING'] }).includes(:brewer)
 	end
 
 end
