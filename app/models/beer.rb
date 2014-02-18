c58dfbbc7e815e43248ac4e35827cd628a740f98
greigm <greig@hamiltron.net>
2014-02-18 14:42:09 +1300

    Mess with CSS a little

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -24,7 +24,7 @@ class Beer < ActiveRecord::Base
 	end
 
 	def self.ontap
-		@beers = self.joins(:location,:brewer).where(locations: { status: ['LOW','SERVING'] }).select("beers.id, brewers.name AS brewer_name, beers.name, beers.price, locations.name AS currently")
+		@beers = self.joins(:location,:brewer).where(locations: { status: ['LOW','SERVING'] }).select("beers.id, brewers.name AS brewer_name, beers.name, beers.price, locations.name AS currently, 2.811 AS tapmarkup, 2 AS offmarkup")
 	end
 
 end
